7fb9cc8ff5ea5ad79982510de7350f541e9259ba
Your Name <you@example.com>
2023-03-21 10:59:11 +0000

    commit message

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+
+  onClickButton = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">
+          The Button has been clicked <span className="count">{count}</span>{' '}
+          times
+        </h1>
+
+        <p className="para">Click the button to increase the count!</p>
+        <button onClick={this.onClickButton} type="button" className="button">
+          Click Me!
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
